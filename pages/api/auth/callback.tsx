7d1e16e9de78bfad7fc356687df7c1d8dbe3acb9
ravislr-beep <ravislr@gmail.com>
2025-09-13 14:40:59 +0100

    Update callback.tsx

--- pages/api/auth/callback.tsx
+++ pages/api/auth/callback.tsx
@@ -1,7 +1,8 @@
 //import type { NextApiRequest, NextApiResponse } from 'next';
-import { supabase } from '@lib/supabaseClient';
+
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { supabase } from '@lib/supabaseClient';
 
 export default function Callback() {
   const router = useRouter()
