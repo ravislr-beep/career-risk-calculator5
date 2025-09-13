40212bad9a17815e9275f2e2327f0a967a41e939
ravislr-beep <ravislr@gmail.com>
2025-09-13 10:52:01 +0100

    Update callback.ts

--- pages/api/auth/callback.ts
+++ pages/api/auth/callback.ts
@@ -1,6 +1,6 @@
 // pages/api/auth/callback.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { supabase } from '../../lib/supabaseClient.ts';
+import { supabase } from '../../lib/supabaseClient';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
